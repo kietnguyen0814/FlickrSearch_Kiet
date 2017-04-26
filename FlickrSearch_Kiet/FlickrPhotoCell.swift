4b39d1051707b1aa984d6c36b82747b55c2f0d50
Group 9 - iOS (HCMUTE) - GST (FSOFT) <iosute@icloud.com>
2017-04-26 16:08:23 +0700

    Flickr Search

---
+++ FlickrSearch_Kiet/FlickrPhotoCell.swift
@@ -0,0 +1,15 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch_Kiet
+//
+//  Created by Cntt01 on 4/26/17.
+//  Copyright © 2017 Group9_UTE. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
---
+++ FlickrSearch_Kiet/FlickrSearchResults.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrSearchResults.swift
+//  FlickrSearch_Kiet
+//
+//  Created by Cntt01 on 4/26/17.
+//  Copyright © 2017 Group9_UTE. All rights reserved.
+//
+
+import Foundation
+
+struct FlickrSearchResults {
+    let searchTerm : String
+    let searchResults : [FlickrPhoto]
+}
