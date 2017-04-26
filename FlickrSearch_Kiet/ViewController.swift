51e6d6d92ce779db3f4766e21a242962f392304b
Group 9 - iOS (HCMUTE) - GST (FSOFT) <iosute@icloud.com>
2017-04-26 15:20:57 +0700

    Initial Commit

---
+++ FlickrSearch_Kiet/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickrSearch_Kiet
+//
+//  Created by Cntt01 on 4/26/17.
+//  Copyright © 2017 Group9_UTE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
